6f9feefc37faef9d713100709641c5cdc9fe1786
Mehdi <s.m.asgarii1990@gmail.com>
2024-07-12 19:36:41 +0330

    react portfolio page created

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,7 +1,55 @@
-import "./portfolio.scss"
+import "./portfolio.scss";
 
 export default function portfolio() {
   return (
-    <div className='portfolio' id="portfolio" ></div>
-  )
+    <div className="portfolio" id="portfolio">
+      <h1>portfolio</h1>
+      <ul>
+        <li className="active" >Featured</li>
+        <li>Web app</li>
+        <li>Moblie app</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+      {/* cart start */}
+        <div className="item">
+          <img src="assets/web-design.jpg" alt="" />
+          <h3>Banking app</h3>
+        </div>
+      {/* cart end */}
+      {/* cart start */}
+        <div className="item">
+          <img src="assets/web-design.jpg" alt="" />
+          <h3>Banking app</h3>
+        </div>
+      {/* cart end */}
+      {/* cart start */}
+        <div className="item">
+          <img src="assets/web-design.jpg" alt="" />
+          <h3>Banking app</h3>
+        </div>
+      {/* cart end */}
+      {/* cart start */}
+        <div className="item">
+          <img src="assets/web-design.jpg" alt="" />
+          <h3>Banking app</h3>
+        </div>
+      {/* cart end */}
+      {/* cart start */}
+        <div className="item">
+          <img src="assets/web-design.jpg" alt="" />
+          <h3>Banking app</h3>
+        </div>
+      {/* cart end */}
+      {/* cart start */}
+        <div className="item">
+          <img src="assets/web-design.jpg" alt="" />
+          <h3>Banking app</h3>
+        </div>
+      {/* cart end */}
+      </div>
+
+    </div>
+  );
 }

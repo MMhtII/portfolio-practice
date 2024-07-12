dc88383fc7fa0556ea040abdf17fcf26b87bf670
Mehdi <s.m.asgarii1990@gmail.com>
2024-07-12 15:48:44 +0330

    inroduction page

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,7 +1,25 @@
-import "./intro.scss"
+import "./intro.scss";
 
 export default function intro() {
   return (
-    <div className='intro' id="intro" ></div>
-  )
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Mehdi</h1>
+          <h3>
+            Freelance <span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
